c8a36b2e0bc30eba6c349e20008f44933c391b75
unknown <Rabi Gorkhali>
2024-09-06 10:48:51 +0545

    fix: css issues in navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -29,24 +29,24 @@
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link {{ str_contains(Route::currentRouteName(), 'home') ? 'active' : '' }} "
+                                <a style="text-decoration: none; margin-left: 10px;" class=" {{ str_contains(Route::currentRouteName(), 'home') ? 'active' : '' }} "
                                    href="{{ route('home') }}">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link {{ str_contains(Route::currentRouteName(), 'posts') ? 'active' : '' }} "
+                                <a style="text-decoration: none;margin-left: 10px;" class=" {{ str_contains(Route::currentRouteName(), 'posts') ? 'active' : '' }} "
                                    href="{{ route('posts.index') }}">Posts</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link {{ str_contains(Route::currentRouteName(), 'post-categories') ? 'active' : '' }} "
-                                   href="{{ route('post-categories.index') }}">Post Categories</a>
+                                <a  style="text-decoration: none;margin-left: 10px;" class=" {{ str_contains(Route::currentRouteName(), 'post-categories') ? 'active' : '' }} "
+                                   href="{{ route('post-categories.index') }}"> Categories</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link {{ str_contains(Route::currentRouteName(), 'users') ? 'active' : '' }} "
+                                <a style="text-decoration: none;margin-left: 10px;" class=" {{ str_contains(Route::currentRouteName(), 'users') ? 'active' : '' }} "
                                    href="{{ route('users.index') }}" href="{{ route('users.index') }}">Users</a>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                <a style="text-decoration: none;margin-left: 10px;" class="" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
